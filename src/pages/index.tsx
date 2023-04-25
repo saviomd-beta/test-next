0d63e07c1321a59caf70c9b4d36c5cec737bf601
Sávio Mendes <saviomd@users.noreply.github.com>
2023-04-25 01:57:35 -0300

    Update test progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <div className="m-8 text-slate-800">
       <ul>
-        <li>❌ storybook</li>
-        <li>❌ deploy gha</li>
-        <li>❌ deploy storybook</li>
+        <li>✅ storybook</li>
+        <li>✅ deploy gha</li>
+        <li>✅ deploy storybook</li>
       </ul>
       <hr />
       <small>✅ 👷 ❌</small>
